77f277e4afe6ad66681b6a72971024759feb7f19
KrushnaGoel <147684359+KrushnaGoel@users.noreply.github.com>
2024-06-23 23:58:13 +0530

    UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:spotify/main_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MainScreen(),
-  ));
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system, // Set theme mode to system
+      theme: ThemeData.light(), // Default theme (light theme)
+      darkTheme: ThemeData.dark(), // Dark theme
+      home: MainScreen(),
+    );
+  }
 }
