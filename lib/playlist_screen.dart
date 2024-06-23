759d20291e9bb9bb7c3a16b2f708d3774dc9d0e8
KrushnaGoel <krushnagoel@gmail.com>
2024-06-23 12:40:44 +0530

    Added functionality to delete songs and minor UI changes

--- lib/playlist_screen.dart
+++ lib/playlist_screen.dart
@@ -8,15 +8,14 @@ import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:spotify/database_helper.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+
 class PlaylistScreen extends StatefulWidget {
   final String playlistName;
   final List<Song> tracks;
-  final List<Song> downloadedSongs;
 
   PlaylistScreen({
     required this.playlistName,
     required this.tracks,
-    required this.downloadedSongs,
   });
 
   @override
@@ -25,26 +24,37 @@ class PlaylistScreen extends StatefulWidget {
 
 class _PlaylistScreenState extends State<PlaylistScreen> {
   Set<String> downloadedTrackIds = {};
+  List<Song> downloadedSongs = [];
   static String ytKey = dotenv.env['youtube_key']!;
   final String _baseURL = 'www.googleapis.com';
-  String token = '';
 
   @override
   void initState() {
     super.initState();
-    // Initialize downloadedTrackIds with already downloaded songs
-    downloadedTrackIds =
-        widget.downloadedSongs.map((track) => track.name).toSet();
+    _loadDownloadedSongs();
   }
 
-  Future _downloadSong(Song track) async {
+  Future<void> _loadDownloadedSongs() async {
+    final dbHelper = DatabaseHelper();
+    final songs = await dbHelper.getSongs();
+    setState(() {
+      downloadedSongs = songs;
+      downloadedTrackIds = songs.map((track) => _createSongId(track)).toSet();
+    });
+  }
+
+  String _createSongId(Song song) {
+    return '${song.name}_${song.artists.join(', ')}';
+  }
+
+  Future<void> _downloadSong(Song track) async {
     String videoID = '';
     try {
       final String q = '${track.name} ${track.artists.first}';
       final Map<String, String> parameters = {
         'part': 'snippet',
         'q': q,
-        'key': ytKey
+        'key': ytKey,
       };
 
       Uri uri = Uri.https(_baseURL, '/youtube/v3/search', parameters);
@@ -66,17 +76,12 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
     }
     try {
       var ytExplode = YoutubeExplode();
-      // var video = await ytExplode.videos.get(videoID);
       var manifest = await ytExplode.videos.streamsClient.getManifest(videoID);
       var streamInfo = manifest.audioOnly.withHighestBitrate();
       var audioStream = ytExplode.videos.streamsClient.get(streamInfo);
 
-      print("success");
-
-      //implement file saving logic here
-      //  await stream.pipe(audioStream);
       final directory = await getApplicationDocumentsDirectory();
-      final filePath = path.join(directory.path, '${track.name}.mp3');
+      final filePath = path.join(directory.path, _createSongId(track) + '.mp3');
       final file = File(filePath);
       await file.create(recursive: true);
       final fileSink = file.openWrite();
@@ -87,7 +92,6 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
 
       ytExplode.close();
 
-
       // Save song data to database
       final dbHelper = DatabaseHelper();
       await dbHelper.insertSong(track);
@@ -96,19 +100,17 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
       throw e;
     }
     setState(() {
-      downloadedTrackIds.add(track.name);
-      widget.downloadedSongs.add(track);
+      downloadedTrackIds.add(_createSongId(track));
+      downloadedSongs.add(track);
     });
   }
 
-  Future _downloadAllSongs() async {
-    setState(() {
-      for (var track in widget.tracks) {
-        if (!downloadedTrackIds.contains(track.name)) {
-          _downloadSong(track);
-        }
+  Future<void> _downloadAllSongs() async {
+    for (var track in widget.tracks) {
+      if (!downloadedTrackIds.contains(_createSongId(track))) {
+        await _downloadSong(track);
       }
-    });
+    }
   }
 
   @override
@@ -118,9 +120,12 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
       appBar: AppBar(
         title: Text(widget.playlistName),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.download),
+          TextButton(
             onPressed: _downloadAllSongs,
+            child: const Text(
+              'Download All',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
@@ -128,7 +133,7 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
         itemCount: widget.tracks.length,
         itemBuilder: (context, index) {
           final track = widget.tracks[index];
-          final isDownloaded = downloadedTrackIds.contains(track.name);
+          final isDownloaded = downloadedTrackIds.contains(_createSongId(track));
           return ListTile(
             title: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -137,8 +142,8 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
                 Text(
                   track.artists.join(', '),
                   style: TextStyle(
-                    color: Colors.grey[600], // Lighter color for artist names
-                    fontSize: 14.0, // Smaller font size for artist names
+                    color: Colors.grey[600],
+                    fontSize: 14.0,
                   ),
                 ),
               ],
